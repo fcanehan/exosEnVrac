408cb99a07def39af88a5efa55d8b75a6930f7c5
fabien canehan <fabien@beweb-16.176N>
2018-03-09 15:12:35 +0100

    modifications 1

---
+++ com/beweb/lunel/programmation/exosEnVrac/algo/Exercice1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beweb.lunel.programmation.exosEnVrac.algo;
+
+/**
+ *initialiser 2 variables booléennes "empty" et "fully"
+ * @author fabien
+ */
+public class Exercice1 {
+  
+    public static void launch (){  
+        boolean empty = false;
+        boolean fully = true;
+        
+        System.out.println("exercice 1 done");
+        
+        }
+
+}
